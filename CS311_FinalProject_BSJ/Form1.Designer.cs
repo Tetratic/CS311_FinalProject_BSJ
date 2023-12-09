f8791a363f532022fab2a9304b3b01b555d41d7d
Tetratic <b.johnston2999@yahoo.com>
2023-12-09 03:42:14 -0500

    Add project files.

---
+++ CS311_FinalProject_BSJ/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace CS311_FinalProject_BSJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtLeft = new TextBox();
+            txtRight = new TextBox();
+            lblLeft = new Label();
+            lblRight = new Label();
+            lblConversion = new Label();
+            lblConvEqn = new Label();
+            cmbFormat = new ComboBox();
+            lblSigFigs = new Label();
+            pnlConvSelect = new Panel();
+            rdoKgLb = new RadioButton();
+            rdoKmMiles = new RadioButton();
+            rdoCF = new RadioButton();
+            rdoGOz = new RadioButton();
+            rdoMFt = new RadioButton();
+            btnHelp = new Button();
+            lblEquals = new Label();
+            chkLR = new CheckBox();
+            pnlConvSelect.SuspendLayout();
+            SuspendLayout();
+            // 
+            // txtLeft
+            // 
+            txtLeft.Location = new Point(65, 131);
+            txtLeft.Name = "txtLeft";
+            txtLeft.Size = new Size(259, 23);
+            txtLeft.TabIndex = 0;
+            // 
+            // txtRight
+            // 
+            txtRight.Location = new Point(468, 131);
+            txtRight.Name = "txtRight";
+            txtRight.Size = new Size(259, 23);
+            txtRight.TabIndex = 1;
+            // 
+            // lblLeft
+            // 
+            lblLeft.AutoSize = true;
+            lblLeft.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblLeft.Location = new Point(65, 69);
+            lblLeft.Name = "lblLeft";
+            lblLeft.Size = new Size(135, 47);
+            lblLeft.TabIndex = 2;
+            lblLeft.Text = "Meters";
+            // 
+            // lblRight
+            // 
+            lblRight.AutoSize = true;
+            lblRight.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblRight.Location = new Point(468, 69);
+            lblRight.Name = "lblRight";
+            lblRight.Size = new Size(90, 47);
+            lblRight.TabIndex = 3;
+            lblRight.Text = "Feet";
+            // 
+            // lblConversion
+            // 
+            lblConversion.AutoSize = true;
+            lblConversion.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblConversion.Location = new Point(65, 205);
+            lblConversion.Name = "lblConversion";
+            lblConversion.Size = new Size(162, 37);
+            lblConversion.TabIndex = 4;
+            lblConversion.Text = "Conversion:";
+            // 
+            // lblConvEqn
+            // 
+            lblConvEqn.AutoSize = true;
+            lblConvEqn.Font = new Font("Segoe UI", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            lblConvEqn.Location = new Point(65, 252);
+            lblConvEqn.Name = "lblConvEqn";
+            lblConvEqn.Size = new Size(104, 25);
+            lblConvEqn.TabIndex = 5;
+            lblConvEqn.Text = "lblConvEqn";
+            // 
+            // cmbFormat
+            // 
+            cmbFormat.DisplayMember = "(none)";
+            cmbFormat.FormattingEnabled = true;
+            cmbFormat.Items.AddRange(new object[] { "0", "1", "2", "3", "4", "5" });
+            cmbFormat.Location = new Point(468, 252);
+            cmbFormat.Name = "cmbFormat";
+            cmbFormat.Size = new Size(121, 23);
+            cmbFormat.TabIndex = 6;
+            // 
+            // lblSigFigs
+            // 
+            lblSigFigs.AutoSize = true;
+            lblSigFigs.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblSigFigs.Location = new Point(468, 205);
+            lblSigFigs.Name = "lblSigFigs";
+            lblSigFigs.Size = new Size(198, 37);
+            lblSigFigs.TabIndex = 7;
+            lblSigFigs.Text = "Decimal Places";
+            // 
+            // pnlConvSelect
+            // 
+            pnlConvSelect.Controls.Add(rdoKgLb);
+            pnlConvSelect.Controls.Add(rdoKmMiles);
+            pnlConvSelect.Controls.Add(rdoCF);
+            pnlConvSelect.Controls.Add(rdoGOz);
+            pnlConvSelect.Controls.Add(rdoMFt);
+            pnlConvSelect.Location = new Point(65, 310);
+            pnlConvSelect.Name = "pnlConvSelect";
+            pnlConvSelect.Size = new Size(662, 116);
+            pnlConvSelect.TabIndex = 8;
+            // 
+            // rdoKgLb
+            // 
+            rdoKgLb.AutoSize = true;
+            rdoKgLb.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rdoKgLb.Location = new Point(382, 70);
+            rdoKgLb.Name = "rdoKgLb";
+            rdoKgLb.Size = new Size(161, 25);
+            rdoKgLb.TabIndex = 3;
+            rdoKgLb.TabStop = true;
+            rdoKgLb.Text = "Kilograms/Pounds";
+            rdoKgLb.UseVisualStyleBackColor = true;
+            // 
+            // rdoKmMiles
+            // 
+            rdoKmMiles.AutoSize = true;
+            rdoKmMiles.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rdoKmMiles.Location = new Point(133, 70);
+            rdoKmMiles.Name = "rdoKmMiles";
+            rdoKmMiles.Size = new Size(153, 25);
+            rdoKmMiles.TabIndex = 1;
+            rdoKmMiles.TabStop = true;
+            rdoKmMiles.Text = "Kilometers/Miles";
+            rdoKmMiles.UseVisualStyleBackColor = true;
+            // 
+            // rdoCF
+            // 
+            rdoCF.AutoSize = true;
+            rdoCF.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rdoCF.Location = new Point(256, 20);
+            rdoCF.Name = "rdoCF";
+            rdoCF.Size = new Size(152, 25);
+            rdoCF.TabIndex = 2;
+            rdoCF.TabStop = true;
+            rdoCF.Text = "Celsius/Farenheit";
+            rdoCF.UseVisualStyleBackColor = true;
+            // 
+            // rdoGOz
+            // 
+            rdoGOz.AutoSize = true;
+            rdoGOz.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rdoGOz.Location = new Point(508, 20);
+            rdoGOz.Name = "rdoGOz";
+            rdoGOz.Size = new Size(135, 25);
+            rdoGOz.TabIndex = 4;
+            rdoGOz.TabStop = true;
+            rdoGOz.Text = "Grams/Ounces";
+            rdoGOz.UseVisualStyleBackColor = true;
+            // 
+            // rdoMFt
+            // 
+            rdoMFt.AutoSize = true;
+            rdoMFt.Checked = true;
+            rdoMFt.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rdoMFt.Location = new Point(34, 20);
+            rdoMFt.Name = "rdoMFt";
+            rdoMFt.Size = new Size(128, 25);
+            rdoMFt.TabIndex = 0;
+            rdoMFt.TabStop = true;
+            rdoMFt.Text = "Meeters/Feet";
+            rdoMFt.UseVisualStyleBackColor = true;
+            // 
+            // btnHelp
+            // 
+            btnHelp.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnHelp.Location = new Point(71, 26);
+            btnHelp.Name = "btnHelp";
+            btnHelp.Size = new Size(114, 40);
+            btnHelp.TabIndex = 9;
+            btnHelp.Text = "Help";
+            btnHelp.UseVisualStyleBackColor = true;
+            btnHelp.Click += btnHelp_Click;
+            // 
+            // lblEquals
+            // 
+            lblEquals.AutoSize = true;
+            lblEquals.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblEquals.Location = new Point(373, 115);
+            lblEquals.Name = "lblEquals";
+            lblEquals.Size = new Size(45, 47);
+            lblEquals.TabIndex = 10;
+            lblEquals.Text = "=";
+            // 
+            // chkLR
+            // 
+            chkLR.AutoSize = true;
+            chkLR.Checked = true;
+            chkLR.CheckState = CheckState.Checked;
+            chkLR.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            chkLR.Location = new Point(65, 174);
+            chkLR.Name = "chkLR";
+            chkLR.Size = new Size(272, 21);
+            chkLR.TabIndex = 11;
+            chkLR.Text = "Conversion Equation uses Left Measure?";
+            chkLR.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(chkLR);
+            Controls.Add(lblEquals);
+            Controls.Add(btnHelp);
+            Controls.Add(pnlConvSelect);
+            Controls.Add(lblSigFigs);
+            Controls.Add(cmbFormat);
+            Controls.Add(lblConvEqn);
+            Controls.Add(lblConversion);
+            Controls.Add(lblRight);
+            Controls.Add(lblLeft);
+            Controls.Add(txtRight);
+            Controls.Add(txtLeft);
+            Name = "Form1";
+            Text = "Unit Converter";
+            pnlConvSelect.ResumeLayout(false);
+            pnlConvSelect.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtLeft;
+        private TextBox txtRight;
+        private Label lblLeft;
+        private Label lblRight;
+        private Label lblConversion;
+        private Label lblConvEqn;
+        private ComboBox cmbFormat;
+        private Label lblSigFigs;
+        private Panel pnlConvSelect;
+        private RadioButton rdoKgLb;
+        private RadioButton rdoKmMiles;
+        private RadioButton rdoCF;
+        private RadioButton rdoGOz;
+        private RadioButton rdoMFt;
+        private Button btnHelp;
+        private Label lblEquals;
+        private CheckBox chkLR;
+    }
+}
---
+++ CS311_FinalProject_BSJ/Form1.cs
@@ -0,0 +1,273 @@
+﻿namespace CS311_FinalProject_BSJ
+{
+    public partial class Form1 : Form
+    {
+        //code run to get the form in its initial state
+        public Form1()
+        {
+            InitializeComponent();
+            txtLeft.Text = "0";
+            txtRight.Text = "0";
+            addListenCtrls();
+            updateForm(getConvBtn(), getLR());
+            cmbFormat.SelectedIndex = 2;
+        }
+        //variable declaration, including String arrays to hold Label text
+        public int chkBtn;
+        public String[,] lblVals = new String[,] {
+            { "Meters", "Feet" },
+            { "Kilometers", "Miles" },
+            { "Celsius", "Farenheit" },
+            { "Kilograms", "Pounds" },
+            { "Grams", "Ounces" }
+        };
+        public String[] convEqns1 = new String[]
+        {
+            "1 Meter ≈ 3.28084 Feet",
+            "1 Kilometer ≈ 0.621371 Miles",
+            "°C * (9/5) +32 = °F",
+            "1 Kilogram ≈ 2.20462 Pounds",
+            "1 Gram ≈ 0.035274 Ounces"
+        };
+        public String[] convEqns2 = new String[]
+        {
+            "0.3048 Meters ≈ 1 Foot",
+            "1.60934 Kilometers ≈ 1 Mile",
+            "°C = (°F − 32) * 5/9",
+            "0.453592 Kilograms ≈ 1 Pound",
+            "28.3495 Grams ≈ 1 Ounce"
+        };
+
+        //adds custom event handling on specific events
+        private void addListenCtrls()
+        {
+            rdoMFt.CheckedChanged += new EventHandler(checks_CheckedChanged);
+            rdoKmMiles.CheckedChanged += new EventHandler(checks_CheckedChanged);
+            rdoCF.CheckedChanged += new EventHandler(checks_CheckedChanged);
+            rdoKgLb.CheckedChanged += new EventHandler(checks_CheckedChanged);
+            rdoGOz.CheckedChanged += new EventHandler(checks_CheckedChanged);
+            chkLR.CheckedChanged += new EventHandler(checks_CheckedChanged);
+
+            txtLeft.TextChanged += new EventHandler(txtLR_TextChanged);
+            txtRight.TextChanged += new EventHandler(txtLR_TextChanged);
+        }
+
+        //when a different conversion is requested, update the form to show that
+        //includes labels for textboxes, changing formulas for conversions and the conversion formula displayed
+        private void checks_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sender is RadioButton || sender is CheckBox)
+            {
+                updateForm(getConvBtn(), getLR());
+            }
+        }
+
+        //when text is changed within the textbox, send the new value to be converted and updated in the other textbox
+        private void txtLR_TextChanged(object sender, EventArgs e)
+        {
+            if (((TextBox)sender).Modified)
+            {
+                TextBox textBox = (TextBox)sender;
+                if (textBox.TabIndex == 0)
+                {
+                    updateTxtR(getConvBtn(), textBox.Text);
+                }
+                else
+                {
+                    updateTxtL(getConvBtn(), textBox.Text);
+                }
+            }
+        }
+
+        //discovers which radio button is selected to know which conversion to perform
+        private int getConvBtn()
+        {
+            foreach (var item in pnlConvSelect.Controls.OfType<RadioButton>())
+            {
+                if (item.Checked == true)
+                {
+                    chkBtn = item.TabIndex;
+                    break;//leaves loop early when this condition is satisfied
+                }
+                else
+                {
+                    chkBtn = 0;
+                }
+            }
+            return chkBtn;
+        }
+
+        //displays the correct conversion formula depending on which side is preferred
+        private bool getLR()
+        {
+            if (chkLR.Checked == true)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        //number format Strings decided by a combobox control
+        private String getFmt()
+        {
+            switch (cmbFormat.SelectedIndex)
+            {
+                case 0: return "#.";
+                case 1: return "#.#";
+                case 2: return "#.##";
+                case 3: return "#.###";
+                case 4: return "#.####";
+                case 5: return "#.#####";
+                default: return "#.";
+            }
+        }
+
+        //updates the labels on the form, as well as the conversion equation
+        private void updateForm(int rdoTInd, bool chkLR)
+        {
+            lblLeft.Text = lblVals[rdoTInd, 0];
+            lblRight.Text = lblVals[rdoTInd, 1];
+
+            if (chkLR == true)
+                lblConvEqn.Text = convEqns1[rdoTInd];
+            else
+            {
+                lblConvEqn.Text = convEqns2[rdoTInd];
+            }
+        }
+
+        //attempts to parse the value of text in the left textbox, and then converts it
+        //uses both proper conversion formula and proper formatting based on combobox selection
+        private void updateTxtR(int rdoBtn, String val)
+        {
+            double dVal;
+            try
+            {
+                dVal = Double.Parse(val);
+            }
+            catch (FormatException)
+            {
+                dVal = 0;
+            }
+
+            switch (rdoBtn)
+            {
+                case 0: txtRight.Text = convMFt(true, dVal, getFmt()); break;
+                case 1: txtRight.Text = convKmMiles(true, dVal, getFmt()); break;
+                case 2: txtRight.Text = convCF(true, dVal, getFmt()); break;
+                case 3: txtRight.Text = convKgLb(true, dVal, getFmt()); break;
+                case 4: txtRight.Text = convGOz(true, dVal, getFmt()); break;
+                default: txtRight.Text = "0"; break;
+            }
+        }
+
+        //attempts to parse the value of text in the right textbox, and then converts it
+        //uses both proper conversion formula and proper formatting based on combobox selection
+        private void updateTxtL(int rdoBtn, String val)
+        {
+            double dVal;
+            try
+            {
+                dVal = Double.Parse(val);
+            }
+            catch (FormatException)
+            {
+                dVal = 0;
+            }
+
+            switch (rdoBtn)
+            {
+                case 0: txtLeft.Text = convMFt(false, dVal, getFmt()); break;
+                case 1: txtLeft.Text = convKmMiles(false, dVal, getFmt()); break;
+                case 2: txtLeft.Text = convCF(false, dVal, getFmt()); break;
+                case 3: txtLeft.Text = convKgLb(false, dVal, getFmt()); break;
+                case 4: txtLeft.Text = convGOz(false, dVal, getFmt()); break;
+                default: txtLeft.Text = "0"; break;
+            }
+        }
+
+        //conversion formula for meters to feet or vice versa, decided by boolean value
+        private String convMFt(bool LR, double dVal, String fmt)
+        {
+            if (LR)
+            {
+                dVal = dVal * 3.28084;
+            }
+            else
+            {
+                dVal = dVal / 3.28084;
+            }
+            return dVal.ToString(fmt);
+        }
+
+        //conversion formula for kilometers to miles or vice versa, decided by boolean value
+        private String convKmMiles(bool LR, double dVal, String fmt)
+        {
+            if (LR)
+            {
+                dVal = dVal * 0.621371;
+            }
+            else
+            {
+                dVal = dVal / 0.621371;
+            }
+            return dVal.ToString(fmt);
+        }
+
+        //conversion formula for Celsius to Farenheit or vice versa, decided by boolean value
+        private String convCF(bool LR, double dVal, String fmt)
+        {
+            if (LR)
+            {
+                dVal = (dVal * 9 / 5) + 32;
+            }
+            else
+            {
+                dVal = (dVal - 32) * 5 / 9;
+            }
+            return dVal.ToString(fmt);
+        }
+
+        //conversion formula for kilograms to pounds or vice versa, decided by boolean value
+        private String convKgLb(bool LR, double dVal, String fmt)
+        {
+            if (LR)
+            {
+                dVal = dVal * 2.20462;
+            }
+            else
+            {
+                dVal = dVal / 2.20462;
+            }
+            return dVal.ToString(fmt);
+        }
+
+        //conversion formula for grams to ounces or vice versa, decided by boolean value
+        private String convGOz(bool LR, double dVal, String fmt)
+        {
+            if (LR)
+            {
+                dVal = dVal * 0.035274;
+            }
+            else
+            {
+                dVal = dVal / 0.035274;
+            }
+            return dVal.ToString(fmt);
+        }
+
+        //help button with instructions on how to use the app
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Getting Started:\n\n" +
+                "The textboxes below are the places where you will enter values to be converted. " +
+                "You can enter on both the left and right, and the opposite side will update with a converted value. " +
+                "\n\nIf you want to convert between one of the other types of conversions, choose the conversion " +
+                "at the bottom of the page, and then enter your values. \n\nUnderneath the text boxes there are " +
+                "two sections: on the left there is a conversion equation, which will tell you how the calculations to convert " +
+                "your values were done, and a checkbox to swap which side is evaluated to 1. The right " +
+                "holds a dropdown menu where you can select how many decimal places you need in your conversion.");
+
+        }
+    }
+}
